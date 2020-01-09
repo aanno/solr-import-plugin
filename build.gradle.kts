2462aa2211c48c6c227cdd10885f2834c9c182d1
Thomas Pasch <thomas.pasch@gmx.de>
2020-01-09 09:38:58 +0100

    gradle: provide all-in-one jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,17 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     java
     kotlin("jvm") version "1.3.61"
+    // https://imperceptiblethoughts.com/shadow/introduction/
+    id("com.github.johnrengelman.shadow") version "5.2.0"
+}
+
+val myShadow by configurations.creating {
+    extendsFrom(configurations["shadow"], configurations["runtimeClasspath"])
+    exclude("org.apache.solr")
+    exclude("org.apache.tika")
+    exclude("org.restlet.jee")
 }
 
 group = "org.github.aanno.solr.importplugin"
@@ -25,11 +36,31 @@ dependencies {
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_11
 }
+
 tasks {
+
     compileKotlin {
         kotlinOptions.jvmTarget = "11"
     }
+
     compileTestKotlin {
         kotlinOptions.jvmTarget = "11"
     }
+
+    named<ShadowJar>("shadowJar") {
+        manifest.attributes.apply {
+            put("Implementation-Title", "Gradle Jar File Example")
+            put("Implementation-Version", archiveVersion)
+            put("Main-Class", "com.mkyong.DateUtils")
+        }
+
+        baseName = project.name + "-all"
+        // archiveBaseName
+        configurations = listOf(myShadow)
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
 }
+
