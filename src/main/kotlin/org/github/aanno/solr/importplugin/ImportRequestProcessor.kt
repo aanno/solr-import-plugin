1c29aa16812429cf7612e83638f7f7b644bf51fd
Thomas Pasch <thomas.pasch@gmx.de>
2020-01-10 14:57:44 +0100

    fixes

--- src/main/kotlin/org/github/aanno/solr/importplugin/ImportRequestProcessor.kt
+++ src/main/kotlin/org/github/aanno/solr/importplugin/ImportRequestProcessor.kt
@@ -1,15 +1,18 @@
 package org.github.aanno.solr.importplugin
 
 import org.apache.solr.common.SolrException
+import org.apache.solr.common.util.ContentStreamBase
 import org.apache.solr.handler.extraction.ExtractingDocumentLoader
 import org.apache.solr.handler.extraction.ParseContextConfig
 import org.apache.solr.handler.extraction.SolrContentHandlerFactory
+import org.apache.solr.response.SolrQueryResponse
 import org.apache.solr.update.*
 import org.apache.solr.update.processor.UpdateRequestProcessor
 import org.apache.tika.config.TikaConfig
 import org.apache.tika.exception.TikaException
 import org.slf4j.LoggerFactory
 import org.xml.sax.SAXException
+import java.io.File
 import java.io.IOException
 import java.lang.invoke.MethodHandles
 
@@ -23,7 +26,11 @@ class ImportRequestProcessor(next: UpdateRequestProcessor) : UpdateRequestProces
 
     private var extractingDocumentLoader: ExtractingDocumentLoader? = null;
 
+    // TODO (tp)
     private var tikaConfig: TikaConfig? = null
+
+    // TODO (tp)
+    var first = true
 
     init {
         log.warn("ImportRequestProcessor.init")
@@ -60,6 +67,12 @@ class ImportRequestProcessor(next: UpdateRequestProcessor) : UpdateRequestProces
     @Throws(IOException::class)
     override fun processAdd(cmd: AddUpdateCommand?) {
         log.warn("processAdd: $cmd")
+        if (first && cmd != null) {
+            first = false
+            val stream = ContentStreamBase.FileStream(File(
+                    "/home2/tpasch/java/solr-8.3.1/example/exampledocs/solr-word.pdf"))
+            extractingDocumentLoader(cmd).load(cmd.req, SolrQueryResponse(), stream, this)
+        }
         next?.processAdd(cmd)
     }
 
